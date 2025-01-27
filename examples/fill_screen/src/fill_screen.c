4be975d87fefd1ce9eb345d56a768acf2efed093
emiliollbb <emiliollbb@gmail.com>
2025-01-27 19:27:22 +0100

    First working code

---
+++ examples/fill_screen/src/fill_screen.c
@@ -0,0 +1,18 @@
+/*
+ * FillScreen Example
+ * To manually build:
+ *  Use Makefile after compile the lib:
+ *  cd ../..
+ *  make all
+ *  cd examples/fillscreen
+ *  make
+ */
+
+#include <neslib.h>
+
+int main(){
+    load_palette();
+    while(1);
+    
+	return 0;
+}
